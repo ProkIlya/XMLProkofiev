3be0f2e1fb21fbe769cb8ce14b661df2e2c56a2b
Илья Прокофьев <prokil1805@gmail.com>
2025-04-23 18:24:16 +0300

    Изменил main.ts

--- example-nestjs/src/main.ts
+++ example-nestjs/src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Настройка сервера для обслуживания статических файлов
+  app.useStaticAssets(path.join(__dirname, '..', 'public'));
+
   app.enableCors({
     origin: 'http://127.0.0.1:5500', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
