ce5ddb9df750add8e4616f49af56606b26176b81
Илья Прокофьев <prokil1805@gmail.com>
2025-04-23 17:27:12 +0300

    Обновил конфиг

--- lab3files/webpack.config.js
+++ lab3files/webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+// Абсолютный путь до целевой папки public в Lab4
+const publicPath = path.resolve('C:/Users/123/ПСП/-5-41-/example-nestjs/public');
+
 module.exports = {
   entry: './main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'example-nestjs/public'), // Убрали ../
+    path: publicPath, // Путь к папке public в Lab4
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         { 
           from: 'index.html',
-          to: 'example-nestjs/public/index.html' // Убрали ../
+          to: path.join(publicPath, 'index.html')
         },
         { 
           from: 'node_modules/bootstrap/dist',
-          to: 'example-nestjs/public/bootstrap' // Убрали ../
+          to: path.join(publicPath, 'bootstrap')
         }
       ]
     })
