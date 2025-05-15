cff484523b7491e8d6dcce58a5dbaae1023392e7
Илья Прокофьев <prokil1805@gmail.com>
2025-05-15 09:27:06 +0300

    Переделал под расширение

--- example-nestjs/src/main.ts
+++ example-nestjs/src/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://127.0.0.1:5500', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
